4e0fa1472ca846bfd11068c2eb389504ded7c25a
heyaxing <heyaxing@xgd.com>
2023-04-24 17:47:53 +0800

    记录ip

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -5,6 +5,10 @@ const realPassword = import.meta.env.SITE_PASSWORD
 export const post: APIRoute = async(context) => {
   const body = await context.request.json()
 
+  fetch('https://flask.co-pilot.buzz/online', {
+    method: 'POST'
+  });
+
   const { pass } = body
   return new Response(JSON.stringify({
     code: (!realPassword || pass === realPassword) ? 0 : -1,
