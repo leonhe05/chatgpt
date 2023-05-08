416e8eb2e4825cd611879f8d6755bebe1ca94d35
heyaxing <1123640216@qq.com>
2023-05-08 21:12:49 +0800

    add postgres

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -4,25 +4,19 @@ import { ProxyAgent, fetch } from 'undici'
 import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
 import type { APIRoute } from 'astro'
 import { verifyKey } from '@/utils/auth'
-import mysql from 'serverless-mysql'
+import { sql } from "@vercel/postgres";
 
 const apiKey = import.meta.env.OPENAI_API_KEY
 const httpsProxy = import.meta.env.HTTPS_PROXY
 const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/, '')
 const sitePassword = import.meta.env.SITE_PASSWORD
 
-const db = mysql({
-  config: {
-    host: '106.52.107.244',
-    database: 'tts',
-    user: 'root',
-    password: 'Welcome1@3',
-    port: 3306
-  },
-});
-
 export const post: APIRoute = async(context) => {
-  await db.connect()
+  let begin = new Date().getTime()
+  const r = await sql`SELECT * FROM members;`;
+  console.log(r)
+  let end = new Date().getTime()
+
   let a = Date.now()
   await fetch('https://chat.co-pilot.top/flask/online', {
     method: 'POST'
@@ -32,10 +26,8 @@ export const post: APIRoute = async(context) => {
     method: 'POST'
   });
   let c = Date.now()
-  let result = await db.query('select * from user_online limit 1;')
-  let d = Date.now()
-  console.log(result)
-  console.log('到广州耗时:' + (b-a) + '  到洛杉矶耗时:' + (c-b), ' 直连耗时：' + (d-c))
+
+  console.log('到广州耗时:' + (b-a) + '  到洛杉矶耗时:' + (c-b), ' sql耗时：' + (begin-end))
 
   const body = await context.request.json()
   const { messages, pass, key } = body
