4faf5b96e7e3d0d5bf3325e1785b5651b454a828
heyaxing <heyaxing@xgd.com>
2023-05-09 15:51:30 +0800

    gz forward

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -3,8 +3,6 @@ import { ProxyAgent, fetch } from 'undici'
 // #vercel-end
 import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
 import type { APIRoute } from 'astro'
-import { verifyKey } from '@/utils/auth'
-import { sql } from "@vercel/postgres";
 
 const apiKey = import.meta.env.OPENAI_API_KEY
 const httpsProxy = import.meta.env.HTTPS_PROXY
@@ -12,25 +10,8 @@ const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com'
 const sitePassword = import.meta.env.SITE_PASSWORD
 
 export const post: APIRoute = async(context) => {
-  let begin = new Date().getTime()
-  const r = await sql`SELECT * FROM members;`;
-  console.log(r)
-  let end = new Date().getTime()
-
-  let a = Date.now()
-  await fetch('https://chat.co-pilot.top/flask/online', {
-    method: 'POST'
-  });
-  let b = Date.now()
-  await fetch('https://flask.co-pilot.buzz/online', {
-    method: 'POST'
-  });
-  let c = Date.now()
-
-  console.log('到广州耗时:' + (b-a) + '  到洛杉矶耗时:' + (c-b), ' sql耗时：' + (end-begin))
-
   const body = await context.request.json()
-  const { messages, pass, key } = body
+  const { messages, pass } = body
   if (!messages) {
     return new Response(JSON.stringify({
       error: {
@@ -38,19 +19,14 @@ export const post: APIRoute = async(context) => {
       },
     }), { status: 400 })
   }
-  verifyKey()
-  if (sitePassword && sitePassword !== pass) {
+  if ('hyx123' !== pass) {
     return new Response(JSON.stringify({
       error: {
         message: 'Invalid password.',
       },
     }), { status: 401 })
   }
-  return new Response(JSON.stringify({
-    error: {
-      message: 'Invalid password.',
-    },
-  }), { status: 401 })
+
   const initOptions = generatePayload(apiKey, messages)
   // #vercel-disable-blocks
   if (httpsProxy)
