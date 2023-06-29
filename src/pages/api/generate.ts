a0f2b530de84935bd48f6b18f0f8a310bb49a650
heyaxing <1123640216@qq.com>
2023-06-29 17:29:43 +0800

    美东结点

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -12,7 +12,7 @@ export const post: APIRoute = async(context) => {
   const initOptions = generatePayload(body)
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
-  const response = await fetch(`https://southeastasia.api.speech.microsoft.com/accfreetrial/texttospeech/acc/v3.0-beta1/vcg/speak`, initOptions).catch((err: Error) => {
+  const response = await fetch(`https://eastus.api.speech.microsoft.com/accfreetrial/texttospeech/acc/v3.0-beta1/vcg/speak`, initOptions).catch((err: Error) => {
     return new Response(JSON.stringify({
       error: {
         code: err.name,
